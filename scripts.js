1db8d85831d443f5e56c0be2f6eea7a7f660ad14
annaegorovaa <anju.egorova@gmail.com>
2018-08-03 15:18:56 -0700

    eslint corrected (indent 2 spaces)

--- scripts.js
+++ scripts.js
@@ -3,39 +3,39 @@ const list =[];
 renderList();
 
 function addTask() {
-   let task = document.getElementById('input');
-   let taskValue = task.value;
-   list.push({title: taskValue, done: false});
-   task.value = '';
-   renderList();
+  let task = document.getElementById('input');
+  let taskValue = task.value;
+  list.push({title: taskValue, done: false});
+  task.value = '';
+  renderList();
 }
 
 function renderList() {
-    const ul = document.getElementById('list');
-    let li;
-    let button;
+  const ul = document.getElementById('list');
+  let li;
+  let button;
 
-    ul.innerHTML = '';
+  ul.innerHTML = '';
 
-    list.forEach((item, i) => {
-        li = document.createElement('li');
-        li.innerHTML = item.title;
-        button = document.createElement('button');
-        button.setAttribute('index', i);
-        button.innerHTML = 'Done';
-        button.addEventListener('click', e => {
-           changeDone(e.target.getAttribute('index'));
-        });
-        if (item.done) {
-            li.className = 'done';
-        }
-        li.appendChild(button);
-        ul.appendChild(li);
+  list.forEach((item, i) => {
+    li = document.createElement('li');
+    li.innerHTML = item.title;
+    button = document.createElement('button');
+    button.setAttribute('index', i);
+    button.innerHTML = 'Done';
+    button.addEventListener('click', e => {
+      changeDone(e.target.getAttribute('index'));
     });
+    if (item.done) {
+      li.className = 'done';
+    }
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
 
 }
 
 function changeDone(index) {
-    list[index].done = !list[index].done;
-    renderList();
+  list[index].done = !list[index].done;
+  renderList();
 }
