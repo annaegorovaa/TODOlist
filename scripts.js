1849422171dcce4fcb385fc4356ce84a02f855fa
annaegorovaa <anju.egorova@gmail.com>
2018-08-08 22:03:47 -0700

    add task edit by modal window

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 let list =[];
+let currentIndex;
+const editableTask = document.getElementById('editable-task');
 
 if (localStorage.tasks) {
   list = JSON.parse(localStorage.tasks);
@@ -16,6 +18,23 @@ document.getElementById('button-addon2').addEventListener('click', () => {
   renderList();
 });
 
+document.getElementById('save-btn').addEventListener('click', () => {
+  changeTask(currentIndex, editableTask.value);
+});
+
+function checkboxClickHandler(e) {
+  changeDone(e.target.getAttribute('index'));
+}
+
+function editClickHandler(e) {
+  currentIndex = e.target.getAttribute('index');
+  editableTask.value = list[currentIndex].title;
+}
+
+function delClickHandler(e) {
+  removeTask(e.target.getAttribute('index'));
+}
+
 function renderList() {
   const table = document.getElementById('list');
 
@@ -23,35 +42,41 @@ function renderList() {
 
   list.forEach((item, i) => {
     let tr = document.createElement('tr');
-    let td1 = document.createElement('td');
     let checkbox = document.createElement('i');
     checkbox.className = 'far fa-circle';
     checkbox.setAttribute('index', i);
-    checkbox.addEventListener('click', e => {
-      changeDone(e.target.getAttribute('index'));
-    });
+    checkbox.addEventListener('click', checkboxClickHandler);
     if (item.done) {
       tr.className = 'done';
       checkbox.className = list[i].done ? 'far fa-check-circle' : 'far fa-circle';
     }
+    let td1 = document.createElement('td');
     td1.appendChild(checkbox);
     tr.appendChild(td1);
     let td2 = document.createElement('td');
     td2.innerText = item.title;
     tr.appendChild(td2);
-    table.appendChild(tr);
     let td3 = document.createElement('td');
+    let edit = document.createElement('i');
+    edit.className = 'far fa-edit';
+    edit.setAttribute('data-toggle', 'modal');
+    edit.setAttribute('data-target', '#modal');
+    edit.setAttribute('index', i);
+    edit.addEventListener('click', editClickHandler);
+    td3.appendChild(edit);
+    tr.appendChild(td3);
+    let td4 = document.createElement('td');
     let del = document.createElement('i');
     del.className = 'far fa-trash-alt';
-    del.addEventListener('click',() => {
-      removeTask(i);
-    });
-    td3.appendChild(del);
-    tr.appendChild(td3);
+    del.setAttribute('index', i);
+    del.addEventListener('click', delClickHandler);
+    td4.appendChild(del);
+    tr.appendChild(td4);
+    table.appendChild(tr);
   });
   localStorage.setItem('tasks', JSON.stringify(list));
-
 }
+
 function changeDone(index) {
   list[index].done = !list[index].done;
   renderList();
@@ -59,5 +84,10 @@ function changeDone(index) {
 
 function removeTask(value) {
   list.splice(value, 1);
+  renderList();
+}
+
+function changeTask(index, value) {
+  list[index].title = value;
   renderList();
 }
